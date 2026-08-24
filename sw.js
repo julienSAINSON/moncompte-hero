eb418ec55915cb1a8d893ca41f67ddbf2ac5e260
Julien SAINSON <julien.sainson@gmail.com>
2026-08-24 18:48:05 +0200

    PWA

---
+++ sw.js
@@ -0,0 +1,83 @@
+const CACHE_NAME = "openrhythm-v1";
+const APP_SHELL = [
+    "./",
+    "index.html",
+    "style.css",
+    "manifest.webmanifest",
+    "assets/icons/icon.svg",
+    "js/audio.js",
+    "js/chart.js",
+    "js/note.js",
+    "js/score.js",
+    "js/input.js",
+    "js/renderer.js",
+    "js/game.js",
+    "assets/default/partition.json",
+    "assets/default/saisis%20ton%20sciforma.mp3"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => Promise.all(
+            keys
+                .filter((key) => key !== CACHE_NAME)
+                .map((key) => caches.delete(key))
+        ))
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    const url = new URL(event.request.url);
+
+    if (url.origin !== self.location.origin) {
+        return;
+    }
+
+    if (url.pathname.endsWith("/assets/default/partition.json")) {
+        event.respondWith(networkFirst(event.request));
+        return;
+    }
+
+    event.respondWith(cacheFirst(event.request));
+});
+
+async function networkFirst(request) {
+    try {
+        const response = await fetch(request);
+        const cache = await caches.open(CACHE_NAME);
+        cache.put(request, response.clone());
+        return response;
+    } catch (_) {
+        const cachedResponse = await caches.match(request);
+        return cachedResponse || Response.error();
+    }
+}
+
+async function cacheFirst(request) {
+    const cachedResponse = await caches.match(request);
+
+    if (cachedResponse) {
+        return cachedResponse;
+    }
+
+    const response = await fetch(request);
+
+    if (response.ok) {
+        const cache = await caches.open(CACHE_NAME);
+        cache.put(request, response.clone());
+    }
+
+    return response;
+}
